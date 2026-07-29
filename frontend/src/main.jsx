4fab29eb0d6ccc03840ff6be9a44461c4bd4af1b
Arshad Ansari <aarshadansariabc@gmail.com>
2026-07-29 23:45:38 +0530

    fixing some bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,15 @@ import App from './App.jsx'
 import './Home/home.css'
 import Home from './Home/Home.jsx'
 import AIWorkspace from './SmrtAgents/AiWorkspace.jsx'
+import Dashboard from './dashboard/dashboard.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <Home/>
    <AIWorkspace/>
+   <Dashboard />
 
   </StrictMode>,
 )
