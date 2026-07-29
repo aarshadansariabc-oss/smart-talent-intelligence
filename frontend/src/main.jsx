9236b4657db084567ac17347bc3f9083784284c9
Arshad Ansari <aarshadansariabc@gmail.com>
2026-07-29 23:16:22 +0530

    adding Dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx' 
 import './home/home.css'
 import Home from './home/Home.jsx'
+import Dashboard from './dashboard/dashboard.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <Home />
+   <Dashboard />
   </StrictMode>,
 )
